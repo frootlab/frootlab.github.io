59127c21596d3182224df35e076c98d746b1ca39
Patrick Michl <patrick.michl@gmail.com>
2019-06-09 11:16:22 +0200

    fixes #124

---
+++ assets/js/stylefix.js
@@ -0,0 +1,22 @@
+// Add event listener, to erxecute scripts, when the DOM is ready,
+// i.e. when the document has been parsed
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Fix title-page
+    if (document.querySelector("#front")) {
+
+      // Calc 1rem in pixels
+      var rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+      // Select DOM element
+      var oCont = document.querySelector("#front .content");
+
+      // Fix padding-top
+      var oCrum = document.querySelector("#breadcrumb");
+      oCont.style.paddingTop = (oCrum.clientHeight + rem).toString() + "px";
+
+      // Fix padding-bottom
+      var oBand = document.querySelector("#front .banderole");
+      oCont.style.paddingBottom = (oBand.clientHeight + rem).toString() + "px";
+  }
+});
